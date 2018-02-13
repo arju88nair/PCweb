252bb45dbb0d2255ea3c60aedb15571a0e0c746f
arju88nair <arju88nair@gmail.com>
2018-02-13 21:02:16 +0530

    Weather api substitution and F bug

--- pcweb/context_processors.py
+++ pcweb/context_processors.py
@@ -7,8 +7,6 @@ def add_variable_to_context(request):
     popular=popularCall()
     popular=popular[2:8]
     ticker = latestNews()
-
-
     return {
         'Day': now.strftime("%A") + " . " + now.strftime("%d") + "." + now.strftime("%B") + "." + now.strftime("%Y"),
         'popular':popular,
