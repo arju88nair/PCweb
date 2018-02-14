042e8d2449c1cb1811247b80e2d8b08837af1512
arju88nair <arju88nair@gmail.com>
2018-02-14 15:39:19 +0530

    Completed weather widget

--- pcweb/static/assets/js/main.js
+++ pcweb/static/assets/js/main.js
@@ -20,19 +20,19 @@ $('.js-geolocation').on('click', function() {
 * Austin WOEID: 2357536
 */
 $(document).ready(function() {
-  loadWeather('Seattle',''); //@params location, woeid
+  loadWeather('Bengaluru',''); //@params location, woeid
 });
 
 function loadWeather(location, woeid) {
   $.simpleWeather({
     location: location,
     woeid: woeid,
-    unit: 'f',
+    unit:'c',
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
       html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';
+      html += '<li>'+weather.alt.temp+'&deg;F</li></ul>';
 
       $("#weather").html(html);
     },
